2ce72f8b136e6b03084e74edbfe8f94dfedbb054
YKuts <yuliiakuts@gmail.com>
2024-12-09 16:55:29 +0100

    Fix config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://syrnyk-eng-api.up.railway.app';
 
 export const getImageUrl = (imagePath) => {
     if (!imagePath) return '/assets/default-product.png';
